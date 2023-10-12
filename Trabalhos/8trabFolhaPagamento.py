8678823ab258b4fec991e3c35c44ad9bb151fd48
Pedro <pedrosantos121229@gmail.com>
2023-10-11 22:59:48 -0300

    ta certo agr

--- Trabalhos/8trabFolhaPagamento.py
+++ Trabalhos/8trabFolhaPagamento.py
@@ -16,19 +16,25 @@ class PontoFunc:
         return self.__ano
     
     @property
-    def lanca_faltas(self):
+    def get_nrFaltas(self):
         return self.__nrFaltas
     
     @property
-    def lanca_atrsos(self):
+    def get_nrAtrasos(self):
         return self.__nrAtrasos
-        
+    
+    def lanca_faltas(self, nrFaltas):
+        self.__nrFaltas += nrFaltas
+    
+    def lanca_atrasos(self, nrAtrasos):
+        self.__nrAtrasos += nrAtrasos
+
 
 class Funcionario(ABC):
-    def __init__(self, cod, nome) -> None:
+    def __init__(self, cod, nome):
         self.__cod = cod
         self.__nome = nome
-        self.__pontoMensal = []
+        self.__getPontoMensal = []
 
     @property
     def get_cod(self):
@@ -40,33 +46,29 @@ class Funcionario(ABC):
     
     @property
     def getPontoMensal(self):
-        return self.__pontoMensal
+        return self.__getPontoMensal
     
     def adicionaPonto(self, mes, ano, faltas, atrasos):
-        ponto = {
-            'mes': mes,
-            'ano': ano,
-            'faltas': faltas,
-            'atrasos': atrasos
-        }
-        self.__pontoMensal.append(ponto)
+        self.__getPontoMensal.append(PontoFunc(mes, ano, faltas, atrasos))
 
-    def lancaFaltas(self, mes, ano, faltas):
-        for ponto in self.__pontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                ponto['faltas'] = faltas
+    def lanca_faltas(self, mes, ano, faltas):
+        lista = self.__getPontoMensal
+        
+        for ficha in lista:
+            if ficha.get_mes == mes and ficha.get_ano == ano:
+                ficha.lanca_faltas(ficha.get_nrFaltas + faltas)
+                break
 
-    def lancaAtrasos(self, mes, ano, atrasos):
-        for ponto in self.__pontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                ponto['atrasos'] = atrasos
+    def lanca_atrasos(self, mes, ano, atrasos):
+        lista = self.__getPontoMensal
+        
+        for ficha in lista:
+            if ficha.get_mes == mes and ficha.get_ano == ano:
+                ficha.lanca_atrasos(atrasos)
+                break
 
     def imprimeFolha(self, mes, ano):
-        for ponto in self.__pontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                salario = self.calculaSalario(mes, ano)
-                bonus = self.calculaBonus(mes, ano)
-                print(f'codigo: {self.__cod}\nnome: {self.__nome}\nSalario Liquido: {salario:.2f}\nBonus: {bonus:.2f} ')
+            print(f'codigo: {self.__cod}\nnome: {self.__nome}\nSalario Liquido: {self.calculaSalario(mes, ano):.2f}\nBonus: {self.calculaBonus(mes, ano):.2f} ')
         
 
     @abstractmethod
@@ -99,16 +101,25 @@ class Professor(Funcionario):
         return self.__nroAulas
 
     def calculaSalario(self, mes, ano):
-        for ponto in self.getPontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                nro_faltas = ponto['faltas']
-                return self.__salarioHr * self.__nroAulas - self.__salarioHr * nro_faltas
+        lista = self.getPontoMensal
+        total_faltas = 0
+        for ponto in lista:
+                
+            if ponto.get_mes == mes and ponto.get_ano == ano:
+                total_faltas = ponto.get_nrFaltas
+                break
+                
+        return self.__salarioHr * self.__nroAulas - self.__salarioHr * total_faltas
               
     def calculaBonus(self, mes, ano):
-        for ponto in self.getPontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                nro_atrasos = ponto['atrasos']
-                return (0.1 - nro_atrasos/100) * self.calculaSalario(mes, ano)
+        lista = self.getPontoMensal
+        nro_atrasos = 0
+        for ponto in lista:
+            if ponto.get_mes == mes and ponto.get_ano == ano:
+                nro_atrasos = ponto.get_nrAtrasos
+                break
+    
+        return (0.1 - nro_atrasos/100) * self.calculaSalario(mes, ano)
 
 class TecAdmin(Funcionario):
     def __init__(self, cod, nome,  funcao, salario: float) -> None:
@@ -125,30 +136,41 @@ class TecAdmin(Funcionario):
         return self.__salario
 
     def calculaSalario(self, mes, ano):
-        for ponto in self.getPontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                nro_faltas = ponto['faltas']
-                return self.__salario - (self.__salario / 30) * nro_faltas
-
+        lista = self.getPontoMensal
+        total_faltas = 0
+        for ponto in lista:
+                
+            if ponto.get_mes == mes and ponto.get_ano == ano:
+                total_faltas = ponto.get_nrFaltas
+                break
+                
+        return self.__salario - ((self.__salario/30)* total_faltas)
+    
     def calculaBonus(self, mes, ano):
-        for ponto in self.getPontoMensal:
-            if ponto['mes'] == mes and ponto['ano'] == ano:
-                nro_atrasos = ponto['atrasos']
-                return (0.08 - nro_atrasos/100)* self.calculaSalario(mes, ano) 
+        lista = self.getPontoMensal
+        nro_atrasos = 0
+        for ponto in lista:
+            if ponto.get_mes == mes and ponto.get_ano == ano:
+                nro_atrasos = ponto.get_nrAtrasos
+                break
+        if nro_atrasos > 8:
+            nro_atrasos = 8
+            
+        return ((8 - nro_atrasos)/100) * self.calculaSalario(mes, ano) 
 
 
 if __name__=="__main__":
     funcionarios=[]
     prof = Professor(1,"Joao","Doutor",45.35,32)
     prof.adicionaPonto(4,2021,0,0)
-    prof.lancaFaltas(4,2021,2)
-    prof.lancaAtrasos(4,2021,3)
+    prof.lanca_faltas(4,2021,2)
+    prof.lanca_atrasos(4,2021,3)
     funcionarios.append(prof)
 
     tec = TecAdmin(2,"Pedro","AnalistaContábil",3600)
     tec.adicionaPonto(4,2021,0,0)
-    tec.lancaFaltas(4,2021,3)
-    tec.lancaAtrasos(4,2021,4)
+    tec.lanca_faltas(4,2021,3)
+    tec.lanca_atrasos(4,2021,4)
     funcionarios.append(tec)
     for func in funcionarios:
         func.imprimeFolha(4,2021)
